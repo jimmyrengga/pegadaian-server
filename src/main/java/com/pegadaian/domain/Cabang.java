0e4edcdece5d788ac9c0f823c04f7723c114137c
Febri Ariansyah <ari.febri24@gmail.com>
2016-03-24 14:11:28 +0700

    membuat class java

---
+++ src/main/java/com/pegadaian/domain/Cabang.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pegadaian.domain;
+
+/**
+ *
+ * @author ari
+ */
+public class Cabang {
+    private int kodeBaru;
+    private int kodeLama;
+    private String namaPegadain;
+    private String kelurahan;
+    private String kecamatan;
+    private String kota;
+    private String provinsi;
+    private String telepon;
+    private String status;
+
+    public int getKodeBaru() {
+        return kodeBaru;
+    }
+
+    public void setKodeBaru(int kodeBaru) {
+        this.kodeBaru = kodeBaru;
+    }
+
+    public int getKodeLama() {
+        return kodeLama;
+    }
+
+    public void setKodeLama(int kodeLama) {
+        this.kodeLama = kodeLama;
+    }
+
+    public String getNamaPegadain() {
+        return namaPegadain;
+    }
+
+    public void setNamaPegadain(String namaPegadain) {
+        this.namaPegadain = namaPegadain;
+    }
+
+    public String getKelurahan() {
+        return kelurahan;
+    }
+
+    public void setKelurahan(String kelurahan) {
+        this.kelurahan = kelurahan;
+    }
+
+    public String getKecamatan() {
+        return kecamatan;
+    }
+
+    public void setKecamatan(String kecamatan) {
+        this.kecamatan = kecamatan;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getProvinsi() {
+        return provinsi;
+    }
+
+    public void setProvinsi(String provinsi) {
+        this.provinsi = provinsi;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
